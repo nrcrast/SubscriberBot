2ec15958abd57795b4cc00789123ca61e21ac6a5
Nick Crast <nrcrast@gmail.com>
2016-05-15 18:35:26 -0400

    Cleaning up. Verified that it all actually works. Moving some stuff out into YAML config file

--- Subscriber_Bot.py
+++ Subscriber_Bot.py
@@ -1,13 +1,17 @@
 import praw
 import sqlite3
+import yaml
+import pprint
+import time
 
 class SubScriber:
 
-    def __init__(self):
+    def __init__(self, config):
 
         self.reddit = praw.Reddit(user_agent='python:AutoSubscriber:v0.1 (by /u/elpantalla)')
-        self.reddit.login('Subscriber_Bot', 'password', disable_warning=True)
-        self.conn = sqlite3.connect('C:\\Users\\nrcra\\subscriptions.db')
+        self.reddit.login(config['reddit']['username'], config['reddit']['password'], disable_warning=True)
+        print("DB Path: {}".format(config['databasePath']))
+        self.conn = sqlite3.connect(config['databasePath'])
         self.db = self.conn.cursor()
 
 
@@ -30,23 +34,23 @@ class SubScriber:
         print("In DB: {}".format(self.isUserInDb(user)))
         print("Subscribed: {}".format(self.isAuthorAlreadySubscribed(user,author)))
         if not self.isAuthorAlreadySubscribed( user, author ):
-        	print("Subscribing to {}".format(user))
-        	self.subscribe( user, author )
-        	self.updateLastPost( user )
+            print("Subscribing to {}".format(user))
+            self.subscribe( user, author )
+            self.updateLastPost( user )
 
     def updateLastPost( self, user ):
-    	if self.getLastPost( user ) != None:
-    		self.db.execute("delete from users where user = ?", [str(user)] )
-    		self.db.execute("insert into users(user,lastpostid) values(?,?)", [str(user),str(self.getLastPost(user))] )
-    		self.conn.commit();
+        if self.getLastPost( user ) != None:
+            self.db.execute("delete from users where user = ?", [str(user)] )
+            self.db.execute("insert into users(user,lastpostid) values(?,?)", [str(user),str(self.getLastPost(user))] )
+            self.conn.commit();
 
     def subscribe( self, user, author ):
-    	self.db.execute("insert into subscribers(user, subscriber) values(?, ?)", [str(user), str(author)] )
-    	self.conn.commit()
+        self.db.execute("insert into subscribers(user, subscriber) values(?, ?)", [str(user), str(author)] )
+        self.conn.commit()
 
     def isAuthorAlreadySubscribed( self, user, author ):
-    	users = self.db.execute("select * from subscribers where user = ? and subscriber = ?", [str(user),str(author)] )
-    	return users.fetchone() != None
+        users = self.db.execute("select * from subscribers where user = ? and subscriber = ?", [str(user),str(author)] )
+        return users.fetchone() != None
 
     def isUserInDb( self, user ):
         users = self.db.execute( "select distinct user from subscribers")
@@ -57,58 +61,93 @@ class SubScriber:
 
         return False
 
-    def checkForMentions( self ):
-    	for msg in self.reddit.get_mentions( limit=None ):
-    		print("Found mention by {}: {}".format(msg.author, msg.body))
-    		self.processMention( msg.author, msg.body )
+    def processInbox( self ):
+        for msg in self.reddit.get_unread( limit=None ):
+            # First token should be username
+            splitMsg = msg.body.split(" ")
+            msg.mark_as_read()
+
+            if splitMsg[0] == "/u/Subscriber_Bot":
+                print("Found mention by {}: {}".format(msg.author, msg.body))
+                self.processMention( msg.author, msg.body )
+
+            elif splitMsg[0] == "help":
+                # Reply with help
+                print("Sending help to {}".format(msg.author))
+                self.reddit.send_message(msg.author, "Subscriber_Bot Help", 
+                        """Hi! Glad you're interested in Subscriber_Bot. 
+
+Subscriber_Bot is designed to allow you to be notified whenever a user of interest submits a new post.
+
+How to interact with Subscriber_Bot:
+
+1.) Reply to any post with the phrase: '/u/Subscriber_Bot [user to subscribe to]' (eg. /u/Subscriber_Bot elpantalla to subscribe to elpantalla's posts)
+
+2.) Send /u/Subscriber_Bot a personal message with the same syntax
+
+3.) Send 'help' to Subscriber_Bot to receive this message
+
+                        """)
+
 
 
 
 class Notifier:
 
-    def __init__(self):
+    def __init__(self, config):
 
         self.reddit = praw.Reddit(user_agent='python:AutoSubscriber:v0.1 (by /u/elpantalla)')
-        self.reddit.login('Subscriber_Bot', 'password', disable_warning=True)
-        self.conn = sqlite3.connect('C:\\Users\\nrcra\\subscriptions.db')
+        self.reddit.login(config['reddit']['username'], config['reddit']['password'], disable_warning=True)
+        self.conn = sqlite3.connect(config['databasePath'])
         self.db = self.conn.cursor()
 
     def getSubscribers( self, user ):
-    	return [sub[0] for sub in self.db.execute("select subscriber from subscribers where user = ?",[str(user)])]
+        return [sub[0] for sub in self.db.execute("select subscriber from subscribers where user = ?",[str(user)])]
 
     def getLastSavedPost( self, user ):
-    	return self.db.execute("select lastpostid from users where user = ?",[str(user)])[0]
+        return self.db.execute("select lastpostid from users where user = ?",[str(user)]).fetchone()[0]
 
     def getUsers( self ):
-    	return [user[0] for user in self.db.execute("select user from users")]
+        return [user[0] for user in self.db.execute("select user from users")]
 
     def getNewPosts( self, user ):
-    	lastSavedPost = self.getLastSavedPost( user )
+        lastSavedPost = self.getLastSavedPost( user )
+        redditor = self.reddit.get_redditor( user )
+        newPosts = []
+        for post in redditor.get_submitted(sort='new', time='all', limit=10):
+            if post.id != lastSavedPost:
+                newPosts.append( post.id )
+            else:
+                break
 
-    	redditor = self.reddit.get_redditor( user )
-    	newPosts = []
-        for post in redditor.get_submitted(sort='new', time='all', limit=50):
-        	if post.id != lastSavedPost:
-        		newPosts.append( post.id )
+        return newPosts
 
     def notifySubscribers( self ):
         for user in self.getUsers():
-        	newPosts = self.getNewPosts( user )
+            print("Processing user: {}".format(user))
+            newPosts = self.getNewPosts( user )
 
-        	if newPosts:
-        		# Save the first new post
-        		# TODO nick make a common function class or something. Not currently organized very well.
-        		self.db.execute("delete from users where user = ?", [str(user)] )
-    		    self.db.execute("insert into users(user,lastpostid) values(?,?)", [str(user),str(newPosts[0]] )
-    		    self.conn.commit();
+            if newPosts:
+                print("New Post: {}".format(newPosts[0]))
+                # Save the first new post
+                # TODO nick make a common function class or something. Not currently organized very well.
+                self.db.execute("delete from users where user = ?", [str(user)] )
+                self.db.execute("insert into users(user,lastpostid) values(?,?)", [str(user),str(newPosts[0])])
+                self.conn.commit()
 
-    		    # Notify subscribers
-    		    for subscriber in self.getSubscribers( user ):
-    		    	for post in newPosts:
+                # Notify subscribers
+                for subscriber in self.getSubscribers( user ):
+                    print("Notifying: {}".format(subscriber))
+                    for post in newPosts:
+                        postInfo = self.reddit.get_submission(submission_id = post )
+                        self.reddit.send_message(subscriber, "New post from /u/{} - {}".format(user,postInfo.title), 
+                        "Hi! User /u/{} has posted a new submission: [{}]({})".format(user, postInfo.title, postInfo.permalink))
 
-    		    	 r.send_message(subsriber, "New post from /u/{}".format(user), 
-    		    	 	"Hi! User /u/{} has posted a new submission: {}".format(user, r.get_submission(submission_id = post ).permalink))
+config = yaml.load(file('config.yaml','r'))
+reader = SubScriber(config)
+notifier = Notifier(config)
 
-bot = SubScriber()
-notifier = Notifier()
-print(notifier.getSubscribers('elpantalla'))
+while True:
+    reader.processInbox()
+    notifier.notifySubscribers()
+    time.sleep(10)
